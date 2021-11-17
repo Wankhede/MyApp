b66bae377a385741f173c4a57e35aa980c6925b8
Swapnil <spwankhede007@gmail.com>
2021-11-17 18:47:21 +0530

    able to add item in recycler view and now moving to delete items

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.myapp.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -16,20 +21,120 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
+import androidx.fragment.app.FragmentTransaction
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    var SignInbutton: LinearLayout? = null
+    var cardview2: CardView? = null
+    var cardview1: View? = null
+
+    var frmtxtv : ConstraintLayout? = null
+
+    companion object {
+        private const val RC_SIGN = 100
+
+        private const val TAG = "SIGN_IN_TAG"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(android.R.id.content, NotesFragment()).commit()
-       // setFragment(ProfileFragment())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        SignInbutton = findViewById(R.id.SiginInButtonn)
+        cardview2 = findViewById(R.id.cardView2)
+        cardview1 = findViewById(R.id.view)
+        frmtxtv = findViewById(R.id.frametextview)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        checkUser()
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_idd))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+
+
+
+        binding.SiginInButtonn.setOnClickListener {
+            Log.d(TAG, "onCreate: Google Sign In")
+            val intent = googleSignInClient.signInIntent
+            Log.d(TAG, "onCreate: Google Sign Iyyyyn")
+
+            cardview2!!.isVisible = false
+            cardview1!!.isVisible = false
+            startActivityForResult(intent, RC_SIGN)
+            Log.d(TAG, "onCreate: Google Sign Innnnn")
+
+
+        }
     }
 
-//    private fun setFragment(fragment : Fragment) {
-//        val fm = supportFragmentManager.beginTransaction()
-//        fm.replace(R.id.fragment_container, fragment)
-//        fm.commit()
-//    }
+    private fun checkUser() {
 
+        val fireBaseUser = firebaseAuth.currentUser
+        if (fireBaseUser != null) {
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, NotesFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(TAG, "onActivityResult: Google SignIn Intent result")
+        val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            val account = accountTask.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
+        } catch (e: Exception) {
+            Log.d(TAG, "onActivityResult:  ${e.message} ")
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogle: Begin Firebase auth with Google")
+
+        val crediential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        firebaseAuth.signInWithCredential(crediential)
+            .addOnSuccessListener { authResult ->
+                Log.d(TAG, "firebaseAuthWithGoogle: Loginsuccess")
+                val fireBaseUser = firebaseAuth.currentUser
+                val fireBaseUID = fireBaseUser!!.uid
+                val fireBaseEmail = fireBaseUser!!.email
+                Log.d(TAG, "firebaseAuthWithGoogle: Email - $fireBaseEmail")
+                Log.d(TAG, "firebaseAuthWithGoogle: UID - $fireBaseUID")
+
+                if (authResult.additionalUserInfo!!.isNewUser) {
+                    Log.d(TAG, "firebaseAuthWithGoogle: Account Created...")
+                    Toast.makeText(this, "Account Created...", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d(TAG, "firebaseAuthWithGoogle: Existing User")
+                    Toast.makeText(this, "Account Existing...", Toast.LENGTH_SHORT).show()
+                }
+                val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_container, NotesFragment())
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG, "firebaseAuthWithGoogle: Login Not Success - ${e.message}")
+                Toast.makeText(this, "Login Not Success...", Toast.LENGTH_SHORT).show()
+
+            }
+    }
 }
--- app/src/main/java/com/example/myapp/NotesFragment.kt
+++ app/src/main/java/com/example/myapp/NotesFragment.kt
@@ -1,14 +1,28 @@
 package com.example.myapp
 
+import android.app.Dialog
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.util.Log
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONArray
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,11 +35,32 @@ private const val ARG_PARAM2 = "param2"
  * Use the [NotesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
 class NotesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var recyclerView: RecyclerView? = null
+
+    var add: FloatingActionButton? = null
+    var add_Note_Layout: ConstraintLayout? = null
+    var back: ImageView? = null
+    var backtoact: ImageView? = null
+    var save: Button? = null
+    var delete: ImageView? = null
+    var newidadd: EditText? = null
+    var newdesadd: EditText? = null
+    var MY_SHARED_PREF_NAME = "Note creator"
+    var sharedPreference: SharedPreferences? = null
+    var id: String? = null
+    var dis: String? = null
+    var idArrayList = ArrayList<String>()
+    var desArrayList = ArrayList<String>()
+
+
+    //var idArrayList : ArrayList<String>? = null
+    //var desArrayList : ArrayList<String>? = null
 
 
     override fun onCreateView(
@@ -34,17 +69,82 @@ class NotesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        val v= inflater.inflate(R.layout.fragment_profile, container, false)
+        val v = inflater.inflate(R.layout.fragment_notes, container, false)
 
+        add = v.findViewById(R.id.addbtn)
+        add_Note_Layout = v.findViewById(R.id.add_note_layout)
+        save = v.findViewById(R.id.save_btn)
+        delete = v.findViewById(R.id.delete_btn)
+        newidadd = v.findViewById(R.id.Note_title)
+        newdesadd = v.findViewById(R.id.Note_description)
 
         recyclerView = v.findViewById(R.id.recyclerview)
-        val notesids = listOf<String>("1","2","3","4","5","6","7","8","9","10")
-        val notesdescriptions = listOf<String>("Aello","Bellow","Cellow","Dellow","Eellow","Fellow","Gellow","Hellow","Iellow","Jellow")
+
+        sharedPreference =
+            requireContext().getSharedPreferences("MY_SHARED_PREF_NAME", Context.MODE_PRIVATE)
+
+        idArrayList = getList("noteiddd", requireContext())
+        desArrayList = getList("notedisss", requireContext())
+
+        Log.d(TAG, "onCreateView: get id array " + idArrayList)
+        Log.d(TAG, "onCreateView: get des array " + desArrayList)
+
+        id = sharedPreference!!.getString("noteiddd", "abc")
+        dis = sharedPreference!!.getString("notedisss", "abcdef")
+
+        val notesids = idArrayList
+        val notesdescriptions = desArrayList
+
         recyclerView!!.layoutManager = LinearLayoutManager(context)
         recyclerView!!.adapter = viewAdapter(notesids, notesdescriptions)
+
+        add_Note_Layout!!.visibility = View.GONE
+        save!!.visibility = View.VISIBLE
+
+
+        add!!.setOnClickListener {
+            add_Note_Layout!!.visibility = View.VISIBLE
+        }
+
+        save!!.setOnClickListener {
+            val noteidd = newidadd!!.text.trim().toString()
+            val notediss = newdesadd!!.text.trim().toString()
+
+            if (noteidd.isEmpty() || notediss.isEmpty()) {
+                showCustomDialog(
+                    requireContext(),
+                    R.string.incomplete_information_title,
+                    R.string.incomplete_board_name,
+                    R.string.ok
+                )
+            } else {
+                add_Note_Layout!!.visibility = View.GONE
+
+
+                idArrayList!!.add(noteidd)
+                desArrayList!!.add(notediss)
+                putList("noteiddd", idArrayList!!, requireContext())
+                putList("notedisss", desArrayList!!, requireContext())
+
+                val notesids = idArrayList
+                val notesdescriptions = desArrayList
+
+                recyclerView!!.layoutManager = LinearLayoutManager(context)
+                recyclerView!!.adapter = viewAdapter(notesids, notesdescriptions)
+
+
+                // editor.apply()
+            }
+        }
+
+        recyclerView!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+        }
+
         return v
 
     }
+
 
     companion object {
         /**
@@ -65,4 +165,69 @@ class NotesFragment : Fragment() {
                 }
             }
     }
+
+    fun showCustomDialog(context: Context, title: Int, msg: Int, btn: Int) {
+
+        val dialog = Dialog(context)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.alert_dialog)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val textView = dialog.findViewById(R.id.dialog_title) as TextView
+        textView.setText(title)
+
+        // textView.typeface = REGULAR
+
+        // set values for custom dialog components - text, image and button
+        val text = dialog.findViewById(R.id.dialog_name) as TextView
+        text.setText(msg)
+        //  text.typeface = LIGHT
+
+        val declineButton = dialog.findViewById(R.id.btn_ok) as Button
+        declineButton.setText(btn)
+        //  declineButton.typeface = LIGHT
+        // if decline button is clicked, close the custom dialog
+        declineButton.setOnClickListener {
+            // Close dialog
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun putList(mtname: String, notes: ArrayList<String>, context: Context) {
+
+        //add the value in shared prefence
+        val sPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = sPrefs.edit()
+
+        val jArray = JSONArray(notes)
+        editor.remove(mtname)
+        editor.putString(mtname, jArray.toString())
+        editor.apply()
+    }
+
+    fun getList(mtname: String, context: Context): ArrayList<String> {
+
+        val sPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = sPrefs.edit()
+
+        val array = ArrayList<String>()
+        val jArrayString = sPrefs.getString(mtname, "NOPREFSAVED")
+        //  String jArrayString=AppUtil.getString("mtname",context);
+
+        try {
+            val DataArray = JSONArray(jArrayString)
+            for (p in 0 until DataArray.length()) {
+                array.add(DataArray.getString(p))
+                //   Log.e("data",""+array.get(p));
+            }
+
+        } catch (ignored: Exception) {
+
+        }
+
+        return array
+    }
+
 }
